b98d5fa237b2469f2a44eb994feb132c94916486
Johannes Drobinski <Johannes.drobinski@stud.tu-darmstadt.de>
2026-01-21 18:33:47 +0100

    set Structure

--- analysis/analysis.m
+++ analysis/analysis.m
@@ -1,24 +1,43 @@
-sole_data_path = "subject_data\sole-data";
-vr_data_path = "subject_data\vr_data";
+%% Define base values
+clearvars
 
-n_subjects = 21;
-subject = 3;
-conditions = ["br", "bvr", "vw", "w", "h", "vh"];
-condition = "br";
+% Data structure
+SOLE_DATA_PATH = "subject_data\sole-data";
+VR_DATA_PATH = "subject_data\vr_data";
 
-subject_data_path = sole_data_path + "\" + subject;
+% Experiment related
+N_SUBJECTS = 21;
+CONDITIONS = ["br", "bvr", "vw", "w", "h", "vh"];
 
-% for condition = conditions
-%     data_path = subject_data_path + "\" + condition +".txt";
-%     data = readtable(data_path);
-% end
+%% Define Functions
 
+% Read in sole data for one condition by one subject
+function condition_sole_data = read_condition_sole_data(subject, condition)
 
-data_path = subject_data_path + "\" + condition +".txt";
-T = readtable(data_path);
+    % Data Format:
+    %     1   , 2     , 3     , 4     , 5     , 6     , 7     , 8     , 9     , 10    , 11
+    %     Time, R-heel, R-mid, R-front, R-total, Time, L-total, L-front, L-mid, L-heel, Time
+    % 
+    % Data is also accessable by collumn headers. Thoose change depending on the soles used, therefore access via index is to be preferred.
 
-time = T{:, 1};
-right_total = T{:, 5};
-left_total = T{:, 7};
+    subject_data_path = SOLE_DATA_PATH + "\" + subject;
+    data_path = subject_data_path + "\" + condition +".txt";
 
-plot(time, [right_total, left_total])
+    condition_sole_data = readtable(data_path);
+
+end
+
+% Read in sole data from all trials by one subject
+function subject_sole_data = read_subject_sole_data(subject)
+end
+
+% Read in all sole data
+function complete_sole_data = read_all_sole_data()
+end
+
+% Analysis functions
+% get individual trials
+function trials = seperate_trials(condition_sole_data)
+    % return array of individual trials, only way there included
+end
+
