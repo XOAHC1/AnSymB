597388a5b84ef5c6ebaa81504cec3bbb4e2ae68a
Johannes Drobinski <Johannes.drobinski@stud.tu-darmstadt.de>
2026-02-02 22:53:34 +0100

    can analyse trials, based on right side,

--- analysis/analysis.m
+++ analysis/analysis.m
@@ -33,6 +33,7 @@ function condition_sole_data = read_condition_sole_data(subject, condition)
 
     condition_sole_data.data = data;
     condition_sole_data.steps = get_steps(data);
+    condition_sole_data.trials = get_trials(condition_sole_data);
 
 end
 
@@ -54,22 +55,30 @@ function subject_sole_data = read_subject_sole_data(subject)
     end
 end
 
-function plot_sole_data(data, plotLabel)
+% external use
+function plot_sole_data(condition_data, mark_steps, plotLabel)
     %PLOT_SOLE_DATA Plot sole pressure data from a table
     %
     %   plot_sole_data(data)
     %   plot_sole_data(data, "S01 - br")
 
     % ---- Input checks ----
-    if nargin < 1 || isempty(data)
+    if nargin < 1 || isempty(condition_data)
         error("Input data must be a non-empty table.");
     end
 
-    if nargin < 2
-        plotLabel = "";
+    if nargin < 2 || isempty(mark_steps)
+        mark_steps = false;
     end
 
-    % ---- Column indices (based on your format) ----
+    if nargin < 3 || isempty(plotLabel)
+        plotLabel = "some Data";
+    end
+
+    % get sole data
+    data = condition_data.data;
+
+    % ---- Column indices  ----
     tR = data{:,1};     % Right foot time
     R_front = data{:,2};
     R_mid   = data{:,3};
@@ -87,16 +96,41 @@ function plot_sole_data(data, plotLabel)
 
     tiledlayout(2,1,"TileSpacing","compact")
 
-    mark_steps = true;
-    steps = get_steps(data);
     if mark_steps
+        % get steps
+        steps = condition_data.steps;
+
+        % get step peak times
         steps_r = steps.right;
         n_steps_r = numel(steps_r);
         step_times_r = arrayfun(@(i) steps_r(i).peakTime, 1:n_steps_r);
+        step_types_r = arrayfun(@(i) steps_r(i).step_type, 1:n_steps_r);
+        
+        stamp_indices_r   = step_types_r == "stamp";
+        stamps_r          = step_times_r(stamp_indices_r);
+
+        walking_indices_r = step_types_r == "walking";
+        walking_r         = step_times_r(walking_indices_r);
+
+        turning_indices_r = step_types_r == "turning";
+        turning_r         = step_times_r(turning_indices_r);
+
+
+
         % and for the left
         steps_l = steps.left;
         n_steps_l = numel(steps_l);
         step_times_l = arrayfun(@(i) steps_l(i).peakTime, 1:n_steps_l);
+        step_types_l = arrayfun(@(i) steps_l(i).step_type, 1:n_steps_l);
+
+        stamp_indices_l   = step_types_l == "stamp";
+        stamps_l          = step_times_l(stamp_indices_l);
+
+        walking_indices_l = step_types_l == "walking";
+        walking_l         = step_times_l(walking_indices_l);
+
+        turning_indices_l = step_types_l == "turning";
+        turning_l         = step_times_l(turning_indices_l);
 
 
     end
@@ -106,7 +140,15 @@ function plot_sole_data(data, plotLabel)
     plot(tR, [R_heel R_mid R_front R_total], 'LineWidth', 1.2)
     hold on
     if mark_steps 
-        plot(step_times_r, 500, "Color", "red", "Marker", "+");
+        if ~isempty(walking_r) 
+            plot(walking_r, 500, "Color", "red", "Marker", "+");
+        end
+        if ~isempty(stamps_r) 
+            plot(stamps_r, 550, "Color", "magenta", "Marker", "diamond");
+        end
+        if ~isempty(turning_r) 
+            plot(turning_r, 450, "Color", "green", "Marker", "*");
+        end
     end
     hold off
     grid on
@@ -120,7 +162,15 @@ function plot_sole_data(data, plotLabel)
     plot(tL, [L_heel L_mid L_front L_total], 'LineWidth', 1.2)
     hold on
     if mark_steps 
-        plot(step_times_l, 500, "Color", "red", "Marker", "+");
+        if ~isempty(walking_l)
+            plot(walking_l, 500, "Color", "red", "Marker", "+");
+        end
+        if ~isempty(stamps_l)
+            plot(stamps_l, 550, "Color", "magenta", "Marker", "diamond");
+        end
+        if ~isempty(turning_l)
+            plot(turning_l, 450, "Color", "green", "Marker", "*");
+        end
     end
     hold off
     grid on
@@ -131,19 +181,15 @@ function plot_sole_data(data, plotLabel)
 
 end
 
-% get individual trials
-function trials = seperate_trials(condition_sole_data)
-    % return sturct of individual trials
-
-    % Notes to stump signal:
-    % no rolling pattern
-    % slower than step
-    % more force in mid-foot than usual
-    
-    trials = struct();
+% get individual trials, mark trial by start time and duration. From stamp to turning steps
+% function trials = identify_trials(condition_sole_data)
+%     % return sturct of individual trials
+%     %  mark trials by starting time and duration
+%     % 
+%     trials = struct();
 
 
-end
+% end
 
 function steps_side = get_steps_one_side(t, heel, mid, front, total)
 
@@ -210,6 +256,9 @@ function steps_side = get_steps_one_side(t, heel, mid, front, total)
 
         % Rolling metric: COP smoothness
         steps_side(i).rolling = (mean(cop_vel));
+        
+        % get step type
+        steps_side(i).step_type = step_type(steps_side(i));
 
     end
 
@@ -238,48 +287,161 @@ function steps = get_steps(condition_data)
      
 end
 
-% identify stamps in struct of steps
-function stamps = get_stamps(steps, condition_data)
+% function is_stamp = is_stamp(step)
 
-    get_steps_one_side(steps.right, time, right)
-end
+%     % define thresholds
+%     ROLLING_TH = 0.2 * 1;       % seconds
+%     DURATION_TH = 2 * 1;        % seconds
+%     SLOPE_TH   = 3000 / .7;      % pressure / s
+%     MAX_H_TH   = 0.5  * 100;    % Proportion
 
-function stamps_side = get_stamps_one_side(steps_side, time, total)
-
-    nSteps = numel(steps_side);
+%     % decide
+%     is_stamp = ...
+%         step.heelPropAtMax < MAX_H_TH && ...
+%         -0.1 < step.rolling && step.rolling < ROLLING_TH && ...
+%         step.duration < DURATION_TH && ...
+%         step.maxSlope > SLOPE_TH;
     
-    ROLLING_TH = 0.08 * 5;   % seconds
-    SLOPE_TH   = 3000 / 6;   % pressure / s
-    DUR_TH     = 0.25 * 20;   % seconds
-    MAX_H_TH   = 0.5  * 1;  
-    % MAX_F_TH   = 
+        
+% end
 
-    mark_stamps = true;
-    for i = 1:nSteps
-        steps(i).isStamp = ...
-            steps(i).heelPropAtMax < MAX_H_TH;% && ...
-            % steps(i).maxSlope > SLOPE_TH && ...
-            % steps(i).duration < DUR_TH;
+function step_type = step_type(step)
+
+    % stamp thresholds
+    ROLLING_TH = 0.2 * 1;       % seconds
+    DURATION_TH = 2 * 1;        % seconds
+    SLOPE_TH   = 3000 / .7;      % pressure / s
+    MAX_H_TH   = 0.5  * 100;    % Proportion
+
+    % walking threshlods
+    ROLLING_WALK_TH = 0.9;
+    DURATION_WALK_TH = 1;
+    SLOPE_WALK_TH = [0, 10000];
+
+
+    if ...
+        step.heelPropAtMax < MAX_H_TH && ...
+        -0.1 < step.rolling && step.rolling < ROLLING_TH && ...
+        step.duration < DURATION_TH && ...
+        step.maxSlope > SLOPE_TH
+
+        step_type = "stamp";
+
+    elseif ...
+        step.rolling > ROLLING_WALK_TH && ...
+        step.duration < DURATION_WALK_TH && ...
+        step.maxSlope > SLOPE_WALK_TH(1) && ...
+        step.maxSlope < SLOPE_WALK_TH(2)
+
+        step_type = "walking";
+
+    else 
+        step_type = "turning";
     end
 
-    nStamp = 0;
-    if mark_stamps
-        figure; plot(time, total); hold on
-        for i = 1:nSteps
-            if steps(i).isStamp
-                % s = i
-                nStamp = nStamp + 1
-                x = steps(i).heelPropAtMax
-                plot(steps(i).peakTime, 500, "Color", "red", "Marker", "diamond");
-            end
+    
+    
+end
+
+function trials = get_trials(condition_data)
+
+    % Input: 
+    %   trial_data:
+    %       struct with:
+    %           .data   (table of data, from stamp to turning steps)
+    %           .steps  (metadata around data)
+    %       Limited to time from stamp to turning steps
+
+    trials = struct();
+
+    % cd = condition_data.steps
+
+    % identify trial borders
+    % stamps to start trials
+    stamps_idx = strcmp([condition_data.steps.right.step_type], "stamp");
+    n_stamps = sum(stamps_idx);
+    stamps = condition_data.steps.right(stamps_idx);
+    trial_starts = arrayfun(@(s) stamps(s).peakTime, 1:n_stamps)*100;
+
+    % turning steps, to end trials
+    turning_steps_idx = strcmp([condition_data.steps.right.step_type], "turning");
+    n_turning = sum(turning_steps_idx);
+    turning_steps = condition_data.steps.right(turning_steps_idx);
+    turning_step_times = arrayfun(@(s) turning_steps(s).peakTime, 1:n_turning)*100;
+
+    % match up stamps and turning steps
+    last_trial_end_idx = 1;
+    trial_ends = zeros(n_stamps, 1);
+    for s = 1:n_stamps
+        t = trial_starts(s);
+        while turning_step_times(last_trial_end_idx) < t
+            last_trial_end_idx = last_trial_end_idx + 1;
         end
-        legend()
+        trial_ends(s) = turning_step_times(last_trial_end_idx);
+    end
+
+    % make indices to integers
+    trial_starts = round(trial_starts);
+    trial_ends = round(trial_ends);
+
+    % call analysis for individual trials
+    for i = 1:n_stamps
+        trial_data = condition_data.data(trial_starts(i):trial_ends(i), :);
+        trial = analyse_trial(trial_data);
+        trials(i).n_stride_r = trial.n_stride_r;
+        trials(i).n_stride_l = trial.n_stride_l;
+        trials(i).stride_freq_r = trial.stride_freq_r;
+        trials(i).stride_freq_l = trial.stride_freq_l;
+        trials(i).stride_freq = trial.stride_freq;
+        trials(i).duration = trial.duration;
+        trials(i).start = trial.start;
     end
 
 end
+
+function trial = analyse_trial(trial_data)
+
+    trial = struct();
+    % trial_data = cond_data_data{start:stop, :};
+
+    % get relative time
+    trial_time_abs = trial_data{:, 1};
+    trial_start_time = trial_time_abs(1);
+    trial_time = trial_time_abs - trial_start_time;
+    trial_duration = trial_time(end);
+
+    trial_steps = get_steps(trial_data);
+
+    % stride frequency
+    n_stride_r = numel(trial_steps.right);
+    n_stride_l = numel(trial_steps.left);
+
+    stride_freq_r = 60 / mean(diff([trial_steps.right.peakTime]));
+    stride_freq_l = 60 / mean(diff([trial_steps.left.peakTime]));
+    stride_freq_mean = mean([stride_freq_r * n_stride_r, stride_freq_l * n_stride_l])/ (n_stride_r + n_stride_l);
+
+
+
+    trial(1).n_stride_r = n_stride_r;
+    trial.n_stride_l = n_stride_l;
+    trial.stride_freq_r = stride_freq_r;
+    trial.stride_freq_l = stride_freq_l;
+    trial.stride_freq = stride_freq_mean;
+    trial.duration = trial_duration;
+    trial.start = trial_start_time;
+
+
+end
+% rechter Peak bis rechter peak -> stride
+
+    % 
 
 %% Testing
 
 s_data = read_subject_sole_data(3);
+c_data = s_data.h;
 
-plot_sole_data(s_data.h.data)
+% plot_sole_data(c_data, true);
+
+
+c_data.trials(1)
