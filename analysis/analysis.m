fbdd820c0affc1048b95abf819c0d83115162dfd
Johannes Drobinski <Johannes.drobinski@stud.tu-darmstadt.de>
2026-02-25 23:49:38 +0100

    set save to false

--- analysis/analysis.m
+++ analysis/analysis.m
@@ -1101,7 +1101,7 @@ end
 
 function create_bar_plot(subjects, data, x_labels, ers, fig_title, xl, yl)
     
-    save = true;
+    save = false;
 
     error_bars = true;
     if nargin < 4 || isempty(ers)
